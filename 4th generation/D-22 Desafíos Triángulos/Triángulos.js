7ab694fafa5ae7d85486ab0e838af17edc600cec
Debora Cuestas <91509952+Debora-Cuestas@users.noreply.github.com>
2022-08-28 07:45:20 -0300

    Add files via upload

---
+++ 4th generation/D-22 Desafíos Triángulos/Triángulos.js
@@ -0,0 +1,4 @@
+var espacio = ' ';
+var anadir = '+';
+var linea = espacio.concat(anadir); // para concatenar ' ' con '+'
+var linea2 = espacio.concat(anadir).concat(anadir);  // para concatenar ' ' con '+' con '+'
---
+++ 4th generation/D-22 Funciones parte 2/circulos.js
@@ -0,0 +1,13 @@
+circulo = crear(); // crear un circulo
+
+talle(circulo,200,200); // cambiar el tamaño del circulo
+
+color(circulo,255,0,0); // cambiar el color del circulo
+
+colorAlAzar(circulo); // cambiar el color de el circulo al azar
+
+zIndex(circulo,5); // cambiar el índice-z de el circulo
+
+mover(circulo,50,50); // mover el circulo
+
+repetirMover(circulo,10,10,5); // mover el circulo muchas veces
